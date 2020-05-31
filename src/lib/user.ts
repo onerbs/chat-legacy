511b92317a36a4dbbcf31f6711d4c0dffef69a8a
AAAA <dev@onerbs.com>
2020-05-31 11:24:42 -0500

    Fix signUp

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,40 +1,62 @@
 import md5 from 'js-md5'
 import store, { Action, Store } from './store'
-import { SKIP, STORE__CLEAR, STORE__SAVE } from './actions'
-import { auth, Users, User } from './firebase'
+import { STORE__CLEAR, STORE__SAVE } from './actions'
+import { auth, User, Users } from './firebase'
+import { avatar } from './avatar'
 
 export async function signUp(email: string, password: string): Promise<Action> {
-  const [hash, name] = [md5(email), email.split('@')[0]]
-  return auth.createUserWithEmailAndPassword(email, password)
-    .then(() => Users.doc(hash).set({ hash, name, rooms: [] }))
-    .then(() => ({ type: SKIP, from: 'user.singup' }))
+  console.log('--signup--')
+  const [hash, displayName] = [md5(email), email.split('@')[0]]
+  const new_user: User = {
+    displayName,
+    hash,
+    photoUrl: avatar(hash),
+    rooms: []
+  }
+  try {
+    const credentials = await auth.createUserWithEmailAndPassword(email, password)
+    const uid = credentials.user?.uid || ''
+    if (uid) {
+      console.log(uid, new_user)
+      try {
+        await Users.doc(uid).set(new_user)
+        console.log('Se ha creado el usuario')
+        return { type: STORE__SAVE, state: parse(new_user) }
+      }
+      catch (err) {
+        throw new Error('No se ha creado el usuario\n' + err.message)
+      }
+    }
+    else throw new Error('UID failure')
+  }
+  catch (e) {
+    console.error('ERROR:' + e.message)
+    return { type: STORE__CLEAR }
+  }
 }
 
 export async function signIn(email: string, password: string): Promise<Action> {
-  return auth.signInWithEmailAndPassword(email, password)
-    .then(credentials => {
-      if (credentials.user) return credentials.user.uid
-      else throw new Error("Can't read user credentials.")
-    })
-    .then(uid => Users.doc(uid).get())
-    .then(snapshot => {
-      if (snapshot.exists) return snapshot.data()
-      else throw new Error("The user's document doesn't exist. Please report!")
-    })
-    .then(user => {
-      if (user) return { type: STORE__SAVE, state: parse(user) }
-      else throw new Error("Got no data.")
-    })
-    .catch(e => {
-      console.error(e.message)
-      return { type: STORE__CLEAR }
-    })
+  console.log('--signin--')
+  try {
+    const credentials = await auth.signInWithEmailAndPassword(email, password)
+    if (!credentials.user) throw new Error("Can't read user credentials.")
+    const uid = credentials.user.uid
+    const snapshot = await Users.doc(uid).get()
+    if (!snapshot.exists) throw new Error("The user's document doesn't exist. Please report!")
+    const user = snapshot.data()
+    if (user) return { type: STORE__SAVE, state: parse(user) }
+    else throw new Error("Got no data.")
+  }
+  catch (e) {
+    console.error('ERROR:' + e.message)
+    return { type: STORE__CLEAR }
+  }
 }
 
 function parse(user: User): Store {
-  const current: Store = store.getState()
-  if (user.hash) current.hash = user.hash
-  if (user.name) current.name = user.name
-  if (user.rooms) current.rooms = user.rooms
-  return current
+  const state: Store = store.getState()
+  if (user.hash) state.hash = user.hash
+  if (user.displayName) state.displayName = user.displayName
+  if (user.rooms) state.rooms = user.rooms
+  return state
 }
